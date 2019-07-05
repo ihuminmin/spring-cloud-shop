ea832c61dca22dbe5114aeabaaeed18f915fe907
huminmin <ihuminmin@163.com>
2019-07-05 12:56:04 +0800

    修改

--- shop-item/item-service/src/main/java/cn/shop/item/ItemService.java
+++ shop-item/item-service/src/main/java/cn/shop/item/ItemService.java
@@ -1,9 +1,11 @@
 package cn.shop.item;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringCloudApplication
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ItemService {
     public static void main(String[] args) {
         SpringApplication.run(ItemService.class);
